a495f96d04473ba21786f22f5b35c51a21170ade
gzovkic <g.zovkic@web.de>
2026-02-01 20:07:11 +0100

    finished

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -4,8 +4,86 @@
 #include "B.hpp"
 #include "C.hpp"
 
+Base* generate()
+{
+	int r;
+    
+    r = std::rand() % 3;
+	switch (r) 
+    {
+		case 0:
+			std::cout << BOLD << "Generated A\n" << RESET << std::endl;
+			return new A();
+        
+		case 1:
+			std::cout << BOLD << "Generated B\n" << RESET << std::endl;
+			return new B();
+        
+		case 2:
+			std::cout << BOLD << "Generated C\n" << RESET << std::endl;
+			return new C();
+        
+		default:
+			return nullptr;
+	}
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Unknown type" << std::endl;
+}
+
+void identify(Base& p)
+{
+    try 
+    {
+        A& a = dynamic_cast<A&>(p);
+        (void)a;
+        std::cout << "A" << std::endl;
+        return;
+    } 
+    catch (...) 
+    {}
+
+    try
+    {
+        B& b = dynamic_cast<B&>(p);
+        (void)b;
+        std::cout << "B" << std::endl;
+        return;
+    } 
+    catch (...) 
+    {}
+
+    try
+    {
+        C& c = dynamic_cast<C&>(p);
+        (void)c;
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch (...)
+    {}
+
+    std::cout << "Unknown type" << std::endl;
+}
 
 int main(void)
 {
-	return (0);
+    std::srand(std::time(0));
+
+    Base* ptr = generate();
+        
+    identify(ptr);
+    identify(*ptr);
+        
+    delete ptr;
+    return 0;
 }

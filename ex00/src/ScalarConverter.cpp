83156f2aca8fcf22a5704f1b0815fd2b090cdea0
gzovkic <g.zovkic@web.de>
2026-02-09 12:12:18 +0100

    bombp

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -39,12 +39,32 @@ void    printValue(char c, int i, float f, double d)
 
 void ScalarConverter::convert(const std::string& input)
 {
+    //char
     if(input.length() == 1 && !std::isdigit(input[0]))
     {
-            char c = input[0];
+        char c = input[0];
 
-            printValue(c,static_cast<int>(c),static_cast<float>(c),static_cast<double>(c));
-            return ;
+        printValue(c,static_cast<int>(c),static_cast<float>(c),static_cast<double>(c));
+        return ;
     }
-	
+    else if(input.find('f') != std::string::npos && input.length() > 1) //float
+    {
+        float f = std::atof(input.c_str());
+
+        printValue(static_cast<char>(f), static_cast<int>(f), f, static_cast<double>(f));
+        return ;
+    }
+    else if(input.find('.') != std::string::npos) //double
+    {
+        double d = std::atof(input.c_str());
+        
+        printValue(static_cast<char>(d), static_cast<int>(d), static_cast<float>(d), d);
+        return ;
+    }
+
+    int i = std::atoi(input.c_str());
+        
+    printValue(static_cast<char>(i), i, static_cast<float>(i), static_cast<double>(i));
+    return ;
+    
 }

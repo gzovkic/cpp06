213accca0713e5d2d8708d14254baea8aaf22ec8
gzovkic <g.zovkic@web.de>
2026-02-11 12:43:33 +0100

    fninished?

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -27,17 +27,12 @@ void    printValue(char c, int i, float f, double d)
     else
         std::cout << "char: Non displayable" << std::endl;
 
-    // int 
-    if(i > INT32_MAX || i < INT32_MIN)
-        std::cout << "int: " << "impossible" << std::endl;
+    if (d > INT_MAX || d < INT_MIN || std::isnan(d))
+        std::cout << "int: impossible" << std::endl;
     else
         std::cout << "int: " << i << std::endl;
 
-    // float
-    // if(f > MAXFLOAT || f <)
     std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f" << std::endl;
-
-    // double
     std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
 }
 
@@ -53,19 +48,36 @@ void ScalarConverter::convert(const std::string& input)
     }
     else if(input.find('f') != std::string::npos && input.length() > 1) //float
     {
-        float f = std::atof(input.c_str());
+        float f = std::strtof(input.c_str(), NULL);
 
         printValue(static_cast<char>(f), static_cast<int>(f), f, static_cast<double>(f));
         return ;
     }
     else if(input.find('.') != std::string::npos) //double
     {
-        double d = std::atof(input.c_str());
-        
+        double d = std::strtod(input.c_str(), NULL);
+
         printValue(static_cast<char>(d), static_cast<int>(d), static_cast<float>(d), d);
         return ;
     }
-    int i = std::atoi(input.c_str());
-        
-    printValue(static_cast<char>(i), i, static_cast<float>(i), static_cast<double>(i));
+    char *end;
+    long test = std::strtol(input.c_str(), &end, 10);
+    if(*end != '\0')
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+        return;
+    }
+    if (test <= INT_MAX && test >= INT_MIN) 
+    {
+        int i = static_cast<int>(test);
+        printValue(static_cast<char>(i), i, static_cast<float>(i), static_cast<double>(i));
+    }
+    else 
+    {
+        double d = std::strtod(input.c_str(), NULL);
+        printValue(static_cast<char>(d), static_cast<int>(d), static_cast<float>(d), d);
+    }
 }
